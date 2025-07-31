b9117e29f37a73a09ad2b88bc72009ec2ea1c82c
farhanwaseer <farhanaliwaseer@gmail.com>
2025-07-31 12:40:47 +0500

    //

--- server.js
+++ server.js
@@ -24,15 +24,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
 
 // rest api
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.get('/', (req,res) => {
+  res.send('backend is running port 8080')
+})
 
 const PORT = process.env.PORT;
 
